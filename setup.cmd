3373d740a1a1fbdd94d846ed97c65f6dd88be7a1
Mark Elston <Mark.Elston@advantest.com>
2020-08-25 16:46:12 -0700

    Enhance setup/build

---
+++ setup.cmd
@@ -0,0 +1,10 @@
+npm add react
+npm add react-dom
+npm add @babel/core --only=dev
+npm add @babel/preset-env --only=dev
+npm add @babel/preset-react --only=dev
+npm add babel-preset-env --only=dev
+npm add babel-preset-react --only=dev
+npm install parcel-bundler --only=dev
+npm install mobx
+npm install mobx-react
